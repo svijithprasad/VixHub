b446e209aeb063593b03c0044ddd17081598a898
svijithprasad <svijithprasad@gmail.com>
2023-06-18 14:37:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-bar");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+
+
+
